0c7c66a8c5786eb5a3becd40c3067c89de3caf71
Itisjigar <reach@jigarsinghrathore.com>
2021-07-24 08:46:17 +0530

    make it better

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          GreenTech Revolution is about to come
+        </p>
+        <p>
+          Stay Tuned
         </p>
         <a
           className="App-link"
